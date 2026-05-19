3e7aab81dbea02354f9ca659b1681c925f601cc1
zalakjoishar <zalakjoishar@gmail.com>
2026-05-19 15:49:16 +0530

    19-05-26

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -39,6 +39,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        navigateFallbackDenylist: [/^\/media/, /^\/api/, /^\/admin/],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
